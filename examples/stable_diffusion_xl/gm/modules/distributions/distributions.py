3b805e3f8778a97e5813dfbfb1454744f78a0a5a
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2024-08-01 16:12:23 +0800

    ops.zeros_like_ext/to/mint.zeros_like

--- examples/stable_diffusion_xl/gm/modules/distributions/distributions.py
+++ examples/stable_diffusion_xl/gm/modules/distributions/distributions.py
@@ -18,8 +18,8 @@ class DiagonalGaussianDistribution(nn.Cell):
         std = ops.exp(0.5 * logvar)
         var = ops.exp(logvar)
         if self.deterministic:
-            std = ops.zeros_like_ext(mean)
-            var = ops.zeros_like_ext(mean)
+            std = mint.zeros_like(mean)
+            var = mint.zeros_like(mean)
         return mean, logvar, var, std
 
     def sample(self, input):
