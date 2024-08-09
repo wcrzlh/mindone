fix aclnn interface bug(softmax, Conv2d)

examples/stable_diffusion_xl/gm/modules/attention.py
@@ -72,7 +72,7 @@ class LinearAttention(nn.Cell):
         q, k, v = mint.split(qkv, 1)
         q, k, v = q.squeeze(0), k.squeeze(0), v.squeeze(0)
 
-        k = mint.softmax(k, dim=-1)
+        k = mint.nn.functional.softmax(k, dim=-1)
 
         # context = ops.einsum("bhdn,bhen->bhde", k, v)
         context = ops.BatchMatMul(transpose_b=True)(k, v)  # bhdn  # bhen  # bhde

examples/stable_diffusion_xl/gm/modules/conv2d.py
@@ -48,14 +48,14 @@ class Conv2d(nn.Cell):
 
     def construct(self, x):
         if self.pad_mode == "same" or self.pad_mode == "valid":
-            return ops.extend.conv2d(x, self.weight, self.bias, self.stride, self.pad_mode, self.dilation, self.group)
+            return mint.conv2d(x, self.weight, self.bias, self.stride, self.pad_mode, self.dilation, self.group)
         elif self.pad_mode == "pad":
             if isinstance(self.padding, int):
                 padding = (self.padding, self.padding, self.padding, self.padding)
                 x = ops.pad_ext(x, padding)
-                return ops.extend.conv2d(x, self.weight, self.bias, self.stride, 0, self.dilation, self.group)
+                return mint.conv2d(x, self.weight, self.bias, self.stride, 0, self.dilation, self.group)
             else:
                 x = ops.pad_ext(x, self.padding)
-                return ops.extend.conv2d(x, self.weight, self.bias, self.stride, 0, self.dilation, self.group)
+                return mint.conv2d(x, self.weight, self.bias, self.stride, 0, self.dilation, self.group)
         else:
             raise ValueError(f"Unsupported pad_mode:{self.pad_modes}")

examples/stable_diffusion_xl/gm/modules/diffusionmodules/openaimodel.py
@@ -314,7 +314,7 @@ class QKVAttentionLegacy(nn.Cell):
         # )  # More stable with f16 than dividing afterwards
         weight = ops.bmm((q * scale).transpose(0, 2, 1), (k * scale))  # (b, c, t) -> (b, t, c)  # (b, c, s)
 
-        weight = mint.softmax(weight, dim=-1)
+        weight = mint.nn.functional.softmax(weight, dim=-1)
 
         # a = th.einsum("bts,bcs->bct", weight, v)
         a = ops.bmm(weight, v.transpose(0, 2, 1)).transpose(  # (b, t, s)  # (b, c, s) -> (b, s, c)
@@ -355,7 +355,7 @@ class QKVAttention(nn.Cell):
             (k * scale).view(bs * self.n_heads, ch, length),  # (b, c, s)
         )
 
-        weight = mint.softmax(weight, dim=-1)
+        weight = mint.nn.functional.softmax(weight, dim=-1)
 
         # a = th.einsum("bts,bcs->bct", weight, v.reshape(bs * self.n_heads, ch, length))
         a = ops.bmm(

examples/stable_diffusion_xl/gm/modules/embedders/clip/modeling_clip.py
@@ -108,7 +108,7 @@ class CLIPAttention(nn.Cell):
             attn_weights = attn_weights.view(bsz, self.num_heads, tgt_len, src_len) + causal_attention_mask
             attn_weights = attn_weights.view(bsz * self.num_heads, tgt_len, src_len)
 
-        attn_weights = mint.softmax(attn_weights, dim=-1)
+        attn_weights = mint.nn.functional.softmax(attn_weights, dim=-1)
 
         attn_probs = ops.dropout(attn_weights, p=self.dropout, training=self.training)
 

examples/stable_diffusion_xl/gm/modules/embedders/open_clip/transformer.py
@@ -152,7 +152,7 @@ class Attention(nn.Cell):
                 attn_mask = new_attn_mask
             attn += attn_mask
 
-        attn = mint.softmax(attn, dim=-1)
+        attn = mint.nn.functional.softmax(attn, dim=-1)
         attn = self.attn_drop(attn)
 
         x = ops.bmm(attn, v)