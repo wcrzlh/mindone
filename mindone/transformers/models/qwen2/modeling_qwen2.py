3de8be0dd4678cab8cb40f4a1bec3f35b36f207e
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2024-10-22 17:50:02 +0800

    MiniCPM-V2.6-FA-recompute-qwen2

--- mindone/transformers/models/qwen2/modeling_qwen2.py
+++ mindone/transformers/models/qwen2/modeling_qwen2.py
@@ -1160,10 +1160,11 @@ class Qwen2Model(Qwen2PreTrainedModel):
         # (`recording cudagraph tree for symint key 13`, etc.), which is VERY slow. A workaround is `@torch.compiler.disable`, but this prevents using
         # `fullgraph=True`. See more context in https://github.com/huggingface/transformers/pull/29114
 
-        if self.config._attn_implementation == "flash_attention_2":
-            if attention_mask is not None and 0.0 in attention_mask:
-                return attention_mask
-            return None
+        # FIXME how to deal with attn implementation
+        # if self.config._attn_implementation == "flash_attention_2":
+        if attention_mask is not None and 0.0 in attention_mask:
+            return attention_mask
+        return None
 
         # For SDPA, when possible, we will rely on its `is_causal` argument instead of its `attn_mask` argument, in
         # order to dispatch on Flash Attention 2. This feature is not compatible with static cache, as SDPA will fail
