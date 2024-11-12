8f48c5852fa7e72a3d347a37ec27da82d5c46ee4
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2024-11-12 14:57:57 +0800

    Add generation/trainer from huiyao

--- mindone/transformers/models/qwen2/modeling_qwen2.py
+++ mindone/transformers/models/qwen2/modeling_qwen2.py
@@ -840,7 +840,7 @@ class Qwen2Model(Qwen2PreTrainedModel):
         self.padding_idx = config.pad_token_id
         self.vocab_size = config.vocab_size
 
-        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, self.padding_idx)
+        self.embed_tokens = nn.Embedding(config.vocab_size, config.hidden_size, padding_idx=self.padding_idx)
         self.layers = nn.CellList(
             [Qwen2DecoderLayer(config, layer_idx) for layer_idx in range(config.num_hidden_layers)]
         )
