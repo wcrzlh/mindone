78d2867da489e151223c7b00d525073155e0284f
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2024-11-13 10:26:04 +0800

    Add generation/trainer from huiyao

--- mindone/transformers/models/qwen2/modeling_qwen2.py
+++ mindone/transformers/models/qwen2/modeling_qwen2.py
@@ -29,7 +29,7 @@ from mindspore import nn, ops, Tensor, Parameter
 from mindspore.nn import BCEWithLogitsLoss, CrossEntropyLoss, MSELoss
 
 from ...activations import ACT2FN
-from ...cache_utils import Cache, DynamicCache, StaticCache
+from ...cache_utils import Cache, StaticCache
 from ...modeling_attn_mask_utils import AttentionMaskConverter
 from ...modeling_outputs import (
     BaseModelOutputWithPast,
