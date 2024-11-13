e7d6d0d2bbdde44a210aab36013d966912d1b239
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2024-11-13 10:07:43 +0800

    Add generation/trainer from huiyao

--- mindone/transformers/utils/import_utils.py
+++ mindone/transformers/utils/import_utils.py
@@ -1,57 +1,15 @@
-import importlib.metadata
-import importlib.util
-import json
-import os
-import shutil
-import subprocess
-import sys
-import warnings
-from collections import OrderedDict
-from functools import lru_cache
-from itertools import chain
-from types import ModuleType
-from typing import Any, Tuple, Union
 
-from packaging import version
-from transformers import logging
+from transformers.utils import is_datasets_available
 
-logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
+from ..mindspore_adapter.utils import _is_ascend
 
-# TODO: This doesn't work for all packages (`bs4`, `faiss`, etc.) Talk to Sylvain to see how to do with it better.
-def _is_package_available(pkg_name: str, return_version: bool = False) -> Union[Tuple[bool, str], bool]:
-    # Check if the package spec exists and grab its version to avoid importing a local directory
-    package_exists = importlib.util.find_spec(pkg_name) is not None
-    package_version = "N/A"
-    if package_exists:
-        try:
-            # Primary method to get the package version
-            package_version = importlib.metadata.version(pkg_name)
-        except importlib.metadata.PackageNotFoundError:
-            # Fallback method: Only for "torch" and versions containing "dev"
-            if pkg_name == "torch":
-                try:
-                    package = importlib.import_module(pkg_name)
-                    temp_version = getattr(package, "__version__", "N/A")
-                    # Check if the version contains "dev"
-                    if "dev" in temp_version:
-                        package_version = temp_version
-                        package_exists = True
-                    else:
-                        package_exists = False
-                except ImportError:
-                    # If the package can't be imported, it's not available
-                    package_exists = False
-            else:
-                # For packages other than "torch", don't attempt the fallback and set as not available
-                package_exists = False
-        logger.debug(f"Detected {pkg_name} version: {package_version}")
-    if return_version:
-        return package_exists, package_version
-    else:
-        return package_exists
 
-def is_flash_attn_greater_or_equal_2_10():
-    if not _is_package_available("flash_attn"):
-        return False
+def is_flash_attn_2_available():
+    if _is_ascend():
+        return True
 
-    return version.parse(importlib.metadata.version("flash_attn")) >= version.parse("2.1.0")
+    return False
+
+
+def is_sdpa_available():
+    return False
