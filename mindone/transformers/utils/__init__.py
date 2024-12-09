2d996c79432ff454050f3f41c3c04f28299fee98
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2024-12-09 17:05:07 +0800

    feat(transformers): Add image_utils

--- mindone/transformers/utils/__init__.py
+++ mindone/transformers/utils/__init__.py
@@ -1,1 +1,38 @@
 from .backbone_utils import BackboneMixin
+
+from .generic import (
+    ContextManagers,
+    ExplicitEnum,
+    ModelOutput,
+    PaddingStrategy,
+    TensorType,
+    add_model_info_to_auto_map,
+    add_model_info_to_custom_pipelines,
+    cached_property,
+    can_return_loss,
+    expand_dims,
+    filter_out_non_signature_kwargs,
+    find_labels,
+    flatten_dict,
+    infer_framework,
+    is_numpy_array,
+    is_tensor,
+    is_mindspore_tensor,
+    reshape,
+    squeeze,
+    strtobool,
+    tensor_size,
+    to_numpy,
+    to_py_obj,
+    torch_float,
+    torch_int,
+    transpose,
+    working_or_temp_dir,
+)
+
+from .import_utils import (
+    is_mindspore_available,
+    is_vision_available,
+    get_mindspore_version,
+    requires_backends,
+)
