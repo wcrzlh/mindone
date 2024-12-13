68ed8d9e22e34eb1fe28744be21a44c7da6076cc
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2024-12-13 15:25:44 +0800

    feat(transformers): Add process utils

--- mindone/transformers/utils/__init__.py
+++ mindone/transformers/utils/__init__.py
@@ -38,3 +38,5 @@ from .import_utils import (
 
 FEATURE_EXTRACTOR_NAME = "preprocessor_config.json"
 IMAGE_PROCESSOR_NAME = FEATURE_EXTRACTOR_NAME
+CHAT_TEMPLATE_NAME = "chat_template.json"
+PROCESSOR_NAME = "processor_config.json"
