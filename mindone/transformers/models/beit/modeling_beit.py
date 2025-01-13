123975bf9442316a27ea48be97491d7bae77a8d5
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-01-13 18:04:23 +0800

    feat(transformers): Debug image classification pipe

--- mindone/transformers/models/beit/modeling_beit.py
+++ mindone/transformers/models/beit/modeling_beit.py
@@ -702,19 +702,20 @@ class BeitPreTrainedModel(MSPreTrainedModel):
 
     def _init_weights(self, module):
         """Initialize the weights"""
-        if isinstance(module, (nn.Dense, nn.Conv2d)):
-            # Slightly different from the TF version which uses truncated_normal for initialization
-            # cf https://github.com/pytorch/pytorch/pull/5617
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.bias is not None:
-                module.bias.data.zero_()
-        elif isinstance(module, nn.Embedding):
-            module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
-            if module.padding_idx is not None:
-                module.weight.data[module.padding_idx].zero_()
-        elif isinstance(module, nn.LayerNorm):
-            module.bias.data.zero_()
-            module.weight.data.fill_(1.0)
+        # if isinstance(module, (nn.Dense, nn.Conv2d)):
+        #     # Slightly different from the TF version which uses truncated_normal for initialization
+        #     # cf https://github.com/pytorch/pytorch/pull/5617
+        #     module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+        #     if module.bias is not None:
+        #         module.bias.data.zero_()
+        # elif isinstance(module, nn.Embedding):
+        #     module.weight.data.normal_(mean=0.0, std=self.config.initializer_range)
+        #     if module.padding_idx is not None:
+        #         module.weight.data[module.padding_idx].zero_()
+        # elif isinstance(module, nn.LayerNorm):
+        #     module.bias.data.zero_()
+        #     module.weight.data.fill_(1.0)
+        pass
 
 
 BEIT_START_DOCSTRING = r"""
