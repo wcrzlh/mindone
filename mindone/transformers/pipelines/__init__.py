dce47dae1d5cd7a079e946b5e68500ca293c69fd
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-01-14 14:50:21 +0800

    feat(transformers): Debug image classification pipe

--- mindone/transformers/pipelines/__init__.py
+++ mindone/transformers/pipelines/__init__.py
@@ -22,7 +22,7 @@ from huggingface_hub import model_info
 from transformers.configuration_utils import PretrainedConfig
 from transformers.dynamic_module_utils import get_class_from_dynamic_module
 from transformers.models.auto.feature_extraction_auto import FEATURE_EXTRACTOR_MAPPING, AutoFeatureExtractor
-from transformers.models.auto.image_processing_auto import IMAGE_PROCESSOR_MAPPING, AutoImageProcessor
+from ..models.auto.image_processing_auto import IMAGE_PROCESSOR_MAPPING, AutoImageProcessor
 from transformers.models.auto.processing_auto import PROCESSOR_MAPPING, AutoProcessor
 from transformers.models.auto.tokenization_auto import TOKENIZER_MAPPING, AutoTokenizer
 from transformers.pipelines.audio_classification import AudioClassificationPipeline
