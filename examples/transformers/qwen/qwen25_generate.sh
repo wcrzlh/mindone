aa008191d68ff9b2a3b278c03fc6303139f2e0a8
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-04-09 17:33:20 +0800

    fix bugs

--- examples/transformers/qwen/qwen25_generate.sh
+++ examples/transformers/qwen/qwen25_generate.sh
@@ -1,10 +1,10 @@
 export DEVICE_ID=2
 
-cpus='cat /proc/cpuinfo| grep "processor"| wc -l'
-avg='expr $cpus \/ 8'
-gap='expr $avg \- 1'
-start='expr $DEVICE_ID \* $avg'
-end='expr $start \+ $gap'
+cpus=`cat /proc/cpuinfo| grep "processor"| wc -l`
+avg=`expr $cpus \/ 8`
+gap=`expr $avg \- 1`
+start=`expr $DEVICE_ID \* $avg`
+end=`expr $start \+ $gap`
 cmdopt=$start"-"$end
 echo "start set cpu $cmdopt"
 
