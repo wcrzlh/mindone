a09b2756e8258854376c0c37ae37f259a29b43d9
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-05-19 16:42:50 +0800

    fix bugs

--- mindone/transformers/modeling_utils.py
+++ mindone/transformers/modeling_utils.py
@@ -283,7 +283,7 @@ def load_state_dict(checkpoint_file: Union[str, os.PathLike]):
             import torch
             state_dict_torch = torch.load(checkpoint_file, map_location="cpu", weights_only=True)
             for key in state_dict_torch.keys():
-                state_dict_torch[key] = ms.tensor(state_dict_torch[key])
+                state_dict_torch[key] = ms.tensor(state_dict_torch[key].numpy())
         else:
             raise NotImplementedError(
                 f"Only supports deserialization of weights file in safetensors format, but got {checkpoint_file}"
