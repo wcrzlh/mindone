e77c6d2360070e6391e22252da4890f3c60e289f
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-06-10 20:11:07 +0800

    fix bugs

--- mindone/transformers/pipelines/zero_shot_image_classification.py
+++ mindone/transformers/pipelines/zero_shot_image_classification.py
@@ -141,7 +141,7 @@ class ZeroShotImageClassificationPipeline(Pipeline):
         if "siglip" in self.model.config.model_type:
             tokenizer_default_kwargs.update(padding="max_length", max_length=64, truncation=True)
         tokenizer_default_kwargs.update(tokenizer_kwargs)
-        text_inputs = self.tokenizer(sequences, return_tensors=self.framework, **tokenizer_default_kwargs)
+        text_inputs = ms.tensor(self.tokenizer(sequences, return_tensors="np", **tokenizer_default_kwargs))
         inputs["text_inputs"] = [text_inputs]
         return inputs
 
