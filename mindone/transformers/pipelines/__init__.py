f8a8f802be76895c886d5476ac1e7b25bce3c0ea
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-07-15 15:08:21 +0800

    fix bugs

--- mindone/transformers/pipelines/__init__.py
+++ mindone/transformers/pipelines/__init__.py
@@ -24,7 +24,7 @@ from huggingface_hub import model_info
 from transformers.configuration_utils import PretrainedConfig
 from transformers.dynamic_module_utils import get_class_from_dynamic_module
 from transformers.models.auto.feature_extraction_auto import FEATURE_EXTRACTOR_MAPPING, AutoFeatureExtractor
-from transformers.models.auto.image_processing_auto import IMAGE_PROCESSOR_MAPPING, AutoImageProcessor
+from mindone.transformers.models.auto.image_processing_auto import IMAGE_PROCESSOR_MAPPING, AutoImageProcessor
 from transformers.models.auto.tokenization_auto import TOKENIZER_MAPPING, AutoTokenizer
 from transformers.tokenization_utils import PreTrainedTokenizer
 from transformers.utils import (
