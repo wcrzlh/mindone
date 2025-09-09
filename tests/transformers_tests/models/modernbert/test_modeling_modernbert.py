875e1bc15eb089b85c13d21cfe07e694dcf79599
Chaoran Wei <77485245+wcrzlh@users.noreply.github.com>
2025-09-09 16:48:17 +0800

    add ut for modern bert

--- tests/transformers_tests/models/modernbert/test_modeling_modernbert.py
+++ tests/transformers_tests/models/modernbert/test_modeling_modernbert.py
@@ -132,11 +132,10 @@ model_tester = ModernBertModelTester()
 (
     config,
     input_ids,
-    token_type_ids,
     input_mask,
     sequence_labels,
     token_labels,
-    choice_labels,
+    choice_labels
 ) = model_tester.prepare_config_and_inputs()
 config_has_num_labels = copy.deepcopy(config)
 config_has_num_labels.num_labels = model_tester.num_labels
